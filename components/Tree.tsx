0073491c2bb1ee04a2b7663dc91797af6cd4c517
julianbenegas <julianbenegas99@gmail.com>
2020-02-05 19:18:32 -0300

    Layout advances with tree!

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -1,27 +1,210 @@
-export default () => {
+import Link from 'next/link'
+import moment from 'moment'
+
+interface Event {
+  name: string
+  description: string
+  timestamp: number
+}
+
+interface EventObj {
+  event: Event
+  index: number
+  isNow: false
+}
+
+interface Now {
+  event: { name: string; description: string; timestamp: undefined }
+  index: undefined
+  isNow: true
+}
+
+type Props = EventObj | Now
+
+const Event = ({ event, index, isNow = false }: Props) => {
+  const alignment = isNow ? 'center' : index && index % 2 ? 'right' : 'left'
+
+  let textAlignment = 'center'
+  if (alignment === 'left') textAlignment = 'right'
+  else if (alignment === 'right') textAlignment = 'left'
+
   return (
     <div className="container">
-      <div>
-        <h1>Now</h1>
-        <p>All there is.</p>
+      <div className="dot" />
+      <Link href="">
+        <button>
+          {event.timestamp && (
+            <a>{moment(event.timestamp).format('MMMM Do, YYYY')}</a>
+          )}
+          <h1>{event.name}</h1>
+          <p>{event.description}</p>
+        </button>
+      </Link>
+      <style jsx>{`
+        .container {
+          display: flex;
+          width: 100%;
+          position: relative;
+          text-align: ${textAlignment};
+          margin-bottom: 20px;
+          z-index: ${isNow ? '10' : 'initial'};
+          justify-content: ${alignment === 'right'
+            ? 'flex-end'
+            : alignment === 'left'
+            ? 'flex-start'
+            : 'center'};
+          align-items: ${isNow ? 'flex-end' : 'initial'};
+        }
+        .dot {
+          min-width: 24px;
+          max-width: 24px;
+          width: 24px;
+          min-height: 24px;
+          max-height: 24px;
+          height: 24px;
+          border: 6px solid var(--background-color);
+          background: var(--teal-2);
+          border-radius: 50%;
+          z-index: 10;
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%, 0);
+        }
+        button {
+          text-align: inherit;
+          border: none;
+          max-width: 50%;
+          padding: 3px
+            ${alignment === 'right'
+              ? '0 0 30px'
+              : alignment === 'left'
+              ? '30px 0 0'
+              : '0 26px 0'};
+          cursor: pointer;
+        }
+        a {
+          color: var(--teal-3);
+          font-weight: 500;
+          font-size: var(--fs-sm);
+        }
+        h1 {
+          font-size: var(--fs-xl);
+          color: var(--grey-8);
+          font-weight: 500;
+          margin: 0.25rem 0 0.1rem;
+        }
+        p {
+          font-size: var(--fs-sm);
+          color: var(--grey-6);
+          line-height: var(--lh-tight);
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default () => {
+  const events: Event[] = [
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Excepturi!',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga.',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 4
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 3
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 2
+    },
+    {
+      name: 'La Derecha Diario',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum quae deserunt! Neque rem cum ut obcaecati optio molestias, cumque fuga. Repudiandae dolore esse maxime porro illum aliquid vitae? Excepturi!',
+      timestamp: Date.now() + 1
+    }
+  ]
+  return (
+    <div className="container">
+      <div className="tree">
+        <div className="root" />
+        {events.map((e, i) => (
+          <Event key={e.timestamp} index={i} event={e} isNow={false} />
+        ))}
+        <Event
+          isNow
+          index={undefined}
+          event={{
+            name: 'Now.',
+            description: 'All there is.',
+            timestamp: undefined
+          }}
+        />
       </div>
       <style jsx>{`
         .container {
           width: 100%;
           height: 100vh;
           overflow-y: scroll;
-          padding: 150.2px 40px 0;
+          padding: 150.2px 10% 0;
           display: flex;
           justify-content: center;
           align-items: flex-start;
         }
-        h1 {
-          font-size: var(--fs-lg);
-          color: var(--grey-8);
+        .tree {
+          text-align: center;
+          position: relative;
+          display: flex;
+          flex-direction: column-reverse;
+          align-items: flex-start;
+          justify-content: flex-end;
+          min-height: calc(100vh - 150.2px);
         }
-        p {
-          font-size: var(--fs-md);
-          color: var(--grey-6);
+        .root {
+          height: 100%;
+          width: 4px;
+          background: var(--grey-3);
+          top: 0;
+          z-index: 1;
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
         }
       `}</style>
     </div>
