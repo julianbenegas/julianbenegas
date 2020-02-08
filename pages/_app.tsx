a5a6075c7b945e3dc995e1dd59ee6191da3e71fb
julianbenegas <julianbenegas99@gmail.com>
2020-02-08 20:19:21 -0300

    removed nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,7 @@
 import '../public/styles/globalStyles.css'
-import '../public/styles/nprogress.css'
-import NProgress from 'nprogress'
-import Router from 'next/router'
 import SEO from '../components/SEO'
 import ColorModeProvider from '../context/colorModeContext'
 import FiltersProvider from '../context/filtersContext'
-
-Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
 
 export default ({ Component, pageProps }: any) => {
   return (
