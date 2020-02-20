1b8d7920d6e9db504d997edb6053a343585238b7
julianbenegas <julianbenegas99@gmail.com>
2020-02-20 11:57:11 -0300

    lodashed unused variable

--- pages/api/spr-test.ts
+++ pages/api/spr-test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
     'Cache-Control',
