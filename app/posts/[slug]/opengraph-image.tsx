fbd84e388b68b7f95e13bb4e619d5846e111bc2b
Julian Benegas <julianbenegas99@gmail.com>
2023-12-30 20:47:57 -0300

    radix

--- app/posts/[slug]/opengraph-image.tsx
+++ app/posts/[slug]/opengraph-image.tsx
@@ -1,10 +1,25 @@
 import { basehub } from 'basehub'
 import { ImageResponse } from 'next/og'
-import { grayDark } from '@radix-ui/colors'
+// import { grayDark } from '@radix-ui/colors'
 import { redis } from '~/app/redis'
 
 export const runtime = 'edge'
 export const revalidate = 60
+
+const grayDark = {
+  gray1: '#111111',
+  gray2: '#191919',
+  gray3: '#222222',
+  gray4: '#2a2a2a',
+  gray5: '#313131',
+  gray6: '#3a3a3a',
+  gray7: '#484848',
+  gray8: '#606060',
+  gray9: '#6e6e6e',
+  gray10: '#7b7b7b',
+  gray11: '#b4b4b4',
+  gray12: '#eeeeee',
+}
 
 export default async function PostOG({ params }: { params: { slug: string } }) {
   // // fonts
