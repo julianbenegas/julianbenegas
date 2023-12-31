c23abb40d35f1206e8afe10860ddcbc8475944d0
Julian Benegas <julianbenegas99@gmail.com>
2023-12-30 21:48:50 -0300

    test

--- app/posts/[slug]/opengraph-image-arch.tsx
+++ app/posts/[slug]/opengraph-image-arch.tsx
@@ -4,11 +4,8 @@ export const runtime = 'edge'
 export const revalidate = 60
 
 export default async function PostOG({ params }: { params: { slug: string } }) {
-  return new ImageResponse(
-    <main tw="flex h-full w-full items-center justify-center" />,
-    {
-      width: 1200,
-      height: 630,
-    }
-  )
+  return new ImageResponse(<main style={{ display: 'flex' }} />, {
+    width: 1200,
+    height: 630,
+  })
 }
