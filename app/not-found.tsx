f64c89957ba0d0dceee9fa14a06ece56cca3a833
Julian Benegas <julianbenegas99@gmail.com>
2023-12-31 20:10:47 -0300

    Link doesn't work on 404, odly

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,12 +8,12 @@ export default function NotFound() {
         <h2 className="text-dark-gray12 text-2xl font-medium">Not Found</h2>
         <p className="text-sm text-dark-gray10 text-balance sm:text-nowrap">
           Could not find requested resource.{' '}
-          <Link
+          <a
             href="/"
             className="underline hover:text-dark-gray11 transition-colors"
           >
             Return home
-          </Link>
+          </a>
           .
         </p>
       </div>
