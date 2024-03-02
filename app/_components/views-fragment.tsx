f6fbacf4721e6532e49c42dde908aa6192bae419
Julian Benegas <julianbenegas99@gmail.com>
2024-03-02 01:36:26 -0300

    wip

--- app/_components/views-fragment.tsx
+++ app/_components/views-fragment.tsx
@@ -1,6 +1,6 @@
 import { unstable_noStore } from 'next/cache'
-import { redis } from '../redis'
 import { draftMode } from 'next/headers'
+import { basehub } from 'basehub'
 
 export const ViewsFragment = async ({
   postId,
@@ -12,12 +12,37 @@ export const ViewsFragment = async ({
   unstable_noStore()
   const { isEnabled: isDraftMode } = draftMode()
 
-  let views: null | number = null
+  const viewsQueryPromise = basehub().query({
+    index: {
+      postsSection: {
+        posts: {
+          __args: { filter: { _sys_id: { eq: postId } } },
+          items: {
+            views: true,
+          },
+        },
+      },
+    },
+  })
+
+  let incr = 0
   if (increment && !isDraftMode) {
-    views = await redis.incr(`views:${postId}`)
-  } else {
-    views = await redis.get(`views:${postId}`)
+    await basehub().mutation({
+      transaction: {
+        __args: {
+          data: JSON.stringify({
+            type: 'update',
+            data: { type: 'number:incr', data: { id: postId } },
+          }),
+        },
+      },
+    })
+    incr++
   }
+
+  const views =
+    ((await viewsQueryPromise).index.postsSection.posts.items[0]?.views ?? 0) +
+    incr
 
   return <>{views || '0'}</>
 }
