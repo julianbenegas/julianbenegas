8c804d7cda9b46c8e0577b3806685126c5775152
Julian Benegas <julianbenegas99@gmail.com>
2025-01-23 10:57:35 -0300

    remove unused env vars

--- app/constants.ts
+++ app/constants.ts
@@ -6,22 +6,10 @@ export const vercelEnv = (process.env.NEXT_PUBLIC_VERCEL_ENV ??
   | 'preview'
   | 'production'
 
-if (!process.env.NEXT_PUBLIC_SITE_URL) {
-  throw new Error('NEXT_PUBLIC_SITE_URL is a required enviroment variable.')
-}
-
-if (!process.env.NEXT_PUBLIC_SITE_URL.startsWith('http')) {
-  throw new Error(
-    'NEXT_PUBLIC_SITE_URL must start with http or https (it must be an absolute URL).'
-  )
-}
-
 const urls = {
   development: 'http://localhost:3000',
-  preview: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
-    : process.env.NEXT_PUBLIC_SITE_URL,
-  production: process.env.NEXT_PUBLIC_SITE_URL,
+  preview: `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+  production: `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`,
 } as const
 
 export const siteURL = new URL(urls[vercelEnv])
